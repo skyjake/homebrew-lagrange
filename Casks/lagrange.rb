5b43be8a0057c9a72aa68cfad8c68ac5101a1565
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-12-25 08:24:22 +0200

    Updated for v1.0.1

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,6 +1,6 @@
 cask "lagrange" do
-  version "1.0.0"
-  sha256 "9db762e1795c948eac1431e533f33516e6cc91ca9e1c6af7bb2b3c61ad61fad2"
+  version "1.0.1"
+  sha256 "336a637817a9670cdb835c34e74f7c128ab3bec8dd671602ace5f7d3fe791d1f"
 
   url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz"
   appcast "https://github.com/skyjake/lagrange/releases.atom"
