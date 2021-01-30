260c51fcf6b42a32e202757812a0688cc5f092ee
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-01-30 20:50:50 +0200

    Added CPU check and the arm64 build

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,14 +1,25 @@
 cask "lagrange" do
   version "1.1.0"
-  sha256 "1b39afad63b20799a223ba0de423d526135cfa99ce2471cc255e4d50ead4f6fc"
 
-  url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz"
+  if Hardware::CPU.intel?
+    sha256 "1b39afad63b20799a223ba0de423d526135cfa99ce2471cc255e4d50ead4f6fc"
+
+    url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz"
+
+    depends_on macos: ">= :high_sierra"
+  else
+    sha256 "6befb2199c0b2e0146fdcbb0cc6d937ce8a0a537dddca6065bdb94a480e04840"
+
+    url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos11.0-arm64.tbz"
+
+    depends_on macos: ">= :big_sur"
+  end
+
   appcast "https://github.com/skyjake/lagrange/releases.atom"
+
   name "Lagrange"
   desc "Desktop GUI client for browsing Geminispace"
   homepage "https://gmi.skyjake.fi/lagrange/"
-
-  depends_on macos: ">= :high_sierra"
 
   app "Lagrange.app"
 
