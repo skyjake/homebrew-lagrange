2ee004241d353b88730a2a9e327527656d644642
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-02-03 21:26:58 +0200

    Updated for v1.1.1

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -1,16 +1,16 @@
 cask "lagrange" do
-  version "1.1.0"
+  version "1.1.1"
 
   if Hardware::CPU.intel?
-    sha256 "1b39afad63b20799a223ba0de423d526135cfa99ce2471cc255e4d50ead4f6fc"
+    sha256 "51abde6ea2191bf3f7fb7831ce968ae0f8f0129b84e28b639dd9e7080bd537f2"
 
     url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz"
 
     depends_on macos: ">= :high_sierra"
   else
-    sha256 "6befb2199c0b2e0146fdcbb0cc6d937ce8a0a537dddca6065bdb94a480e04840"
+    sha256 "c4128bd497d01b29ac7f5dfbc2c4cb569595de34cd0a57f70222b15d93d6aa85"
 
-    url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos11.0-arm64.tbz"
+    url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos11.1-arm64.tbz"
 
     depends_on macos: ">= :big_sur"
   end
