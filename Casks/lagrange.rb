4ac4d98bfae4b4f921b5ae24b138db15bdf2b11a
Jaakko Keränen <jaakko.keranen@iki.fi>
2021-02-27 13:05:53 +0200

    Changed download URLs to GitHub

--- Casks/lagrange.rb
+++ Casks/lagrange.rb
@@ -4,13 +4,13 @@ cask "lagrange" do
   if Hardware::CPU.intel?
     sha256 "5013aa5856016979e3bde9ff8ac689569d1ae3055f76a0de6242684a7b38568b"
 
-    url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz"
+    url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos10.13-x86_64.tbz"
 
     depends_on macos: ">= :high_sierra"
   else
     sha256 "f9df9094ef0f6766c5d259bf066ff594c3fd31d7fb65f6d4f231949630b4f88e"
 
-    url "https://git.skyjake.fi/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos11.0-arm64.tbz"
+    url "https://github.com/skyjake/lagrange/releases/download/v#{version}/lagrange_v#{version}_macos11.0-arm64.tbz"
 
     depends_on macos: ">= :big_sur"
   end
